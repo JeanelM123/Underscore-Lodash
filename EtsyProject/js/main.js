e09dc0c6043e210e0b493e7f033cf64fc33af61c
Jeanel 'Nelly Nelle' Mouelle <jeanelmouelle@yahoo.com>
2015-10-12 08:58:13 -0400

    Need Help with CSS and cascading . Linking tags and extensions in scss

--- EtsyProject/js/main.js
+++ EtsyProject/js/main.js
@@ -1,5 +1,16 @@
-(function () {
+((function () {
 
-  console.log('It Works!');
+var templateString = $('#itemTemplate').text();
+
+var templateFunction = _.template(templateString);
+
+//will display requested items 
+
+_.each(etsy.results, function (item) {
+  var itemHTML = templateFunction(item);
+  $('.container').append(itemHTML);
+});
+
 
 }());
+
